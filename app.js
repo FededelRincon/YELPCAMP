d04871595034b637dd22d935621a4c386b046e89
Fede del Rincon <delrincon@gmail.com>
2020-11-07 11:53:34 -0300

    01 node basico y ejs basico

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { get } = require('mongoose');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');  //creo q aca meteria lo de react!!!!!!
+app.set('views', path.join(__dirname, 'views'))
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+
+
+
+
+
+
+app.listen(3000, ()=> {
+    console.log('Serving on port 3000')
+})
