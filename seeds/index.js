14f501eb0418c134f190ab511ab5cc21c030310b
Fede del Rincon <delrincon@gmail.com>
2020-11-09 11:44:29 -0300

    13-417 images add and seed ddbb

--- seeds/index.js
+++ seeds/index.js
@@ -27,9 +27,13 @@ const seedDB = async() => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, cupiditate quidem numquam tenetur pariatur sint quis magnam aperiam aliquam ut sit consequatur dolorum est corrupti in voluptatem nostrum esse hic!',
+            price     //esto va suelto xq chupa del random de arriba 
         })
         await camp.save();
     }
