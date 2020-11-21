c30e5af45f54c37d45fbf948ee2eca7674a025ec
Fede del Rincon <delrincon@gmail.com>
2020-11-21 11:11:36 -0300

    54-522 fancy way to restructure Routes(more small)

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,18 +6,38 @@ const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 const Campground = require('../models/campground');
 
-router.get('/', catchAsync(campgrounds.index));
+//asi estaba original
+
+// router.get('/', catchAsync(campgrounds.index));
+
+// router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+
+// router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+
+// router.get('/:id', catchAsync(campgrounds.showCampground));
+
+// router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderNewForm));
+
+// router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampgrounds));
+
+// router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
+
+//fancy way to change it
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampgrounds))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderNewForm));
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampgrounds));
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 module.exports = router;
